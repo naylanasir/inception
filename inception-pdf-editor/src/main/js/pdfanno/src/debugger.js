75975d906b327119367ef1ef2d4950045f0177dd
Nayla Nasir <nans18@student.bth.se>
2019-10-06 18:48:15 +0200

    Update debugger.js

--- inception-pdf-editor/src/main/js/pdfanno/src/debugger.js
+++ inception-pdf-editor/src/main/js/pdfanno/src/debugger.js
@@ -380,7 +380,7 @@ var Stepper = (function StepperClosure() {
         line.appendChild(c('td', JSON.stringify(simplifyArgs(decArgs))));
       }
       if (operatorsToDisplay < operatorList.fnArray.length) {
-        line = c('tr');
+        //line = c('tr'); removed this assignment
         var lastCell = c('td', '...');
         lastCell.colspan = 4;
         chunk.appendChild(lastCell);
